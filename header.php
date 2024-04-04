a3bc4f927ee7893f8b224511bdcc53b1158dbacf
Imran Hossain <mdimran.khulna@gmail.com>
2024-04-04 14:12:53 +0600

    Header Issue fixed

--- header.php
+++ header.php
@@ -34,7 +34,7 @@
 
         .slider-content {
             padding: 15px;
-			color: 000
+			color: #000;
             background-color: rgba(255, 255, 255, 0.8);
             width: 50%;
             text-align: center;
