8db75afe27cbd0adf77e2ab2b1e7e8a1ded458bf
Imran Hossain <mdimran.khulna@gmail.com>
2024-04-30 15:31:45 +0600

    archive page updated

--- archive.php
+++ archive.php
@@ -20,11 +20,11 @@ get_header();
 							the_archive_title( '<h1 class="page-title">', '</h1>' );
 							the_archive_description( '<div class="archive-description">', '</div>' );
 							?>
-						</header><!-- .page-header -->
+						</header>
 
 						<div class="row">
 						<?php
-						/* Start the Loop */
+						
 						while ( have_posts() ) :
 							the_post();
 
