e53ee9ac7d887d7b652e4af805528adc724ed72d
Imran Hossain <mdimran.khulna@gmail.com>
2025-01-19 10:38:56 +0600

    Header Updated

--- header.php
+++ header.php
@@ -161,8 +161,8 @@
 			</div>
 			<div class="social-links">
 				<ul class="clearfix">
-					<li><a href="#"><span class="fab fa-twitter"></span></a></li>
-					<li><a href="#"><span class="fab fa-facebook-square"></span></a></li>
+					<li><a href="#Twiter"><span class="fab fa-twitter"></span></a></li>
+					<li><a href="#Facebook"><span class="fab fa-facebook-square"></span></a></li>
 					<li><a href="#"><span class="fab fa-pinterest-p"></span></a></li>
 					<li><a href="#"><span class="fab fa-instagram"></span></a></li>
 					<li><a href="#"><span class="fab fa-youtube"></span></a></li>
